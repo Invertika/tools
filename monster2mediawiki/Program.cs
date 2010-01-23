2f2c2a0e7992a499219039ad7634f4bfc6956638
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2010-01-23 14:53:23 +0000

    * Program.cs: Reinfolge der Werte geändert.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@1183 784bf6b6-0765-11df-962c-b5c252e50527

--- monster2mediawiki/Program.cs
+++ monster2mediawiki/Program.cs
@@ -28,14 +28,15 @@ namespace monsters2mediawiki
 				{
 					sw.WriteLine("{| border=\"1\" cellspacing=\"0\" cellpadding=\"5\" width=\"100%\" align=\"center\"\n"
 					+"! style=\"background:#efdead;\" | Bild\n"
-					+"! style=\"background:#efdead;\" | Name\n"
 					+"! style=\"background:#efdead;\" | ID\n"
+					+"! style=\"background:#efdead;\" | Name\n"
 					+"! style=\"background:#efdead;\" | HP\n"
+					+"! style=\"background:#efdead;\" | Agressiv\n"
 					+"! style=\"background:#efdead;\" | Angriff\n"
 					+"! style=\"background:#efdead;\" | Verteidigung (physisch)\n"
 					+"! style=\"background:#efdead;\" | Verteidigung (magisch)\n"
 					+"! style=\"background:#efdead;\" | Erfahrung\n"
-					+"! style=\"background:#efdead;\" | Agressiv\n"
+
 					//+"! style=\"background:#efdead;\" | Drops"
 					+"|-\n");
 
@@ -45,14 +46,10 @@ namespace monsters2mediawiki
 					foreach(Monster monster in monsters)
 					{
 						sw.WriteLine("| align=\"center\" | [[Image:monster-{0}.png]]", monster.ID);
-						sw.WriteLine("| {0}", monster.Name);
 						sw.WriteLine("| align=\"center\" | {0}", monster.ID);
+						sw.WriteLine("| {0}", monster.Name);
 						sw.WriteLine("| align=\"center\" | {0}", monster.Attributes.HP);
-						sw.WriteLine("| align=\"center\" | {0}-{1}", monster.Attributes.AttackMin-monster.Attributes.AttackDelta, monster.Attributes.AttackMin+monster.Attributes.AttackDelta);
-						sw.WriteLine("| align=\"center\" | {0}%", monster.Attributes.PhysicalDefence);
-						sw.WriteLine("| align=\"center\" | {0}%", monster.Attributes.MagicalDefence);
-						sw.WriteLine("| align=\"center\" | {0}", monster.Exp);
-
+						
 						if(monster.Behavior!=null)
 						{
 							if(monster.Behavior.Aggressive) sw.WriteLine("| align=\"center\" | Ja");
@@ -62,6 +59,11 @@ namespace monsters2mediawiki
 						{
 							sw.WriteLine("| align=\"center\" | nicht definiert");
 						}
+						
+						sw.WriteLine("| align=\"center\" | {0}-{1}", monster.Attributes.AttackMin-monster.Attributes.AttackDelta, monster.Attributes.AttackMin+monster.Attributes.AttackDelta);
+						sw.WriteLine("| align=\"center\" | {0}%", monster.Attributes.PhysicalDefence);
+						sw.WriteLine("| align=\"center\" | {0}%", monster.Attributes.MagicalDefence);
+						sw.WriteLine("| align=\"center\" | {0}", monster.Exp);
 
 						//sw.WriteLine("| Bone (2.1%)<br>Skull (3%)<br>Dark Crystal (10%)<br>Warlord Helmet (0.03%)<br>Warlord Plate (0.02%)<br>Leather Gloves (0.35%)");
 						//sw.WriteLine("| ''<span style=\"color:#ad1818\">Aggro</span>''");
