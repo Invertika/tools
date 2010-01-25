6fc3064d5912de27582ea21f11f7bad2f5ee81b7
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2010-01-25 13:59:03 +0000

    Invertika Development Helper hinzugefügt.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@1219 784bf6b6-0765-11df-962c-b5c252e50527

---
+++ Invertika Development Helper/Globals.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSCL;
+
+namespace Invertika_Development_Helper
+{
+    public class Globals
+    {
+        public static string OptionsDirectory=FileSystem.ApplicationDataDirectory+".invertika.org\\Invertika Development Helper\\";
+        public static string OptionsXmlFilename=OptionsDirectory+"Invertika Development Helper.xml";
+        public static XmlData Options;
+    }
+}
