ec27b5de21952f6471d602d650d0e17e5330a0b1
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2010-08-07 19:44:06 +0000

    Autoupdate Webservice hinzugefügt.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@1599 784bf6b6-0765-11df-962c-b5c252e50527

---
+++ autoupdate/webservice.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import string,cgi,time, os
+from os import curdir,sep
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+
+port = 80
+path_autoupdate = "/home/manaserv/autoupdate.py"
+password = "geheim"
+
+class MyHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+            if self.path.endswith("restart?password="+password):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write("Autoupdate erzeugt ein neues Update...")
+                
+                # Starte Autoupdate
+                os.system("python3 autoupdate.py")
+                return
+            return
+
+def main():
+    try:
+        server = HTTPServer(('', port), MyHandler)
+        print 'Starte HTTP Server...'
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print 'Fahre Server herunter...'
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
