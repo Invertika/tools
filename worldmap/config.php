1218558582150bda70e3688cf55e6ad3c70d0d26
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2010-09-29 05:49:23 +0000

    Worldmap wieder zurückverschoben.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@2313 784bf6b6-0765-11df-962c-b5c252e50527

---
+++ worldmap/config.php
@@ -0,0 +1,18 @@
+<?php 
+  $title="Invertika Weltkarte";
+
+  $mappath="http://data.invertika.org/worldmap/";
+  
+  $map_x_min=-25;
+  $map_x_max=25;
+  $map_y_min=-25;
+  $map_y_max=25;
+
+  $initial_zoom=50;
+  
+  //Datenbank
+  $host = 'localhost'; // Datenbank Server - meistens "localhost" kann aber auch eine IP sein
+  $name = 'test'; // Datenbank Name
+  $user = 'testuser'; // Datenbank Benutzer
+  $password = 'geheim'; // Datenbank Passwort
+?>
