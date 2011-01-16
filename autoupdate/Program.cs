1dbb76cf185226717b565c8a2c2113aa52509b95
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-01-16 11:37:05 +0000

    Noch eine Fehlerbehebung an Autoupdate.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@3019 784bf6b6-0765-11df-962c-b5c252e50527

--- autoupdate/Program.cs
+++ autoupdate/Program.cs
@@ -181,8 +181,8 @@ namespace autoupdate
 				sw.WriteLine("##3 Serenity");
 				sw.WriteLine("##0");
 				sw.WriteLine("##0 Entwicklerserver des Invertika Projektes");
-				sw.WriteLine("Jeden Tag wird ein neues Image aus");
-				sw.WriteLine("dem SVN Repository erstellt.");
+				sw.WriteLine("##0 Jeden Tag wird ein neues Image aus");
+				sw.WriteLine("##0 dem SVN Repository erstellt.");
 				sw.WriteLine("##0");
 				sw.WriteLine("##0 Status: in Betrieb");
 				sw.WriteLine("##0 Autoupdate vom {0}, {1} Uhr.", DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString());
