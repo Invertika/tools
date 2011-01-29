49c4c7ba576019e936773f42c3dc6cdabffee2e7
erikschilling@freenet.de <erikschilling@freenet.de@784bf6b6-0765-11df-962c-b5c252e50527>
2011-01-29 10:25:56 +0000

    Abfrage ob eine Änderung vorliegt eingebaut.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@3158 784bf6b6-0765-11df-962c-b5c252e50527

--- Skripte/autoupdate.sh
+++ Skripte/autoupdate.sh
@@ -1,1 +1,15 @@
-while true; do sleep 120; mono autoupdate.exe autoupdate.xml; sleep 3600; done; 
+#!/bin/bash
+while true;
+do
+    sleep 120;
+    REVISION_SERVER=`svn info http://invertika.googlecode.com/svn/trunk | grep Revision|awk '{print $2}'`
+    REVISION_LOCAL=`svn info ~/invertika | grep Revision|awk '{print $2}'` # Pfad anpassen
+    if [ "$REVISION_SERVER" == "$REVISION_LOCAL" ]
+    then
+        #echo "Kein Update"
+    else
+        echo "Update"
+        mono autoupdate.exe autoupdate.xml;
+    fi
+    sleep 3600;
+done; 
