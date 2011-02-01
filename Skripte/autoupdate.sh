1d79f0d7b722616ae595a0dd99deec436f84fb07
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-02-01 18:43:07 +0000

    Skript angepasst auf 10 Minuten Rythmus bei Änderung.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@3274 784bf6b6-0765-11df-962c-b5c252e50527

--- Skripte/autoupdate.sh
+++ Skripte/autoupdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true;
 do
-    sleep 120;
+    sleep 60;
     REVISION_SERVER=`svn info http://invertika.googlecode.com/svn/trunk | grep Revision|awk '{print $2}'`
     REVISION_LOCAL=`svn info ~/invertika | grep Revision|awk '{print $2}'` # Pfad anpassen
     if [ "$REVISION_SERVER" == "$REVISION_LOCAL" ]
@@ -10,6 +10,6 @@ do
     else
         #echo "Update"
         mono autoupdate.exe autoupdate.xml;
-        sleep 3600;
+        sleep 600;
     fi
 done; 
