0eba5cfafafee88a471af50dfd0acdadb13fe894
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-02-01 18:51:46 +0000

    Skript korrigiert.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@3275 784bf6b6-0765-11df-962c-b5c252e50527

--- Skripte/autoupdate.sh
+++ Skripte/autoupdate.sh
@@ -4,11 +4,8 @@ do
     sleep 60;
     REVISION_SERVER=`svn info http://invertika.googlecode.com/svn/trunk | grep Revision|awk '{print $2}'`
     REVISION_LOCAL=`svn info ~/invertika | grep Revision|awk '{print $2}'` # Pfad anpassen
-    if [ "$REVISION_SERVER" == "$REVISION_LOCAL" ]
-    then
-        #echo "Kein Update"
-    else
-        #echo "Update"
+    if [ "$REVISION_SERVER" != "$REVISION_LOCAL" ] 
+      then
         mono autoupdate.exe autoupdate.xml;
         sleep 600;
     fi
