1ba8010ea599b599ea73ab68b7f4db206db07953
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-04-18 17:54:51 +0000

    Typo korrigiert.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4285 784bf6b6-0765-11df-962c-b5c252e50527

--- Skripte/server-monitor.sh
+++ Skripte/server-monitor.sh
@@ -4,14 +4,14 @@
 
 #Config
 FILENAME="/home/manaserv/.manaserv-game.log"
-OLDSIZE=$(stat -c %s $FILENAME) # Datei-größe ermitteln
+OLDSIZE=$(stat -c %s $FILENAME) # Dateigröße ermitteln
 
 #Testschleifen
 while true;
 do
     sleep 300; #5 Minuten
 
-    NEWSIZE=$(stat -c %s $FILENAME) # Datei-größe ermitteln
+    NEWSIZE=$(stat -c %s $FILENAME) # Dateigröße ermitteln
 
     if [ $OLDSIZE == $NEWSIZE ]; then
 	echo "Restart server..."
