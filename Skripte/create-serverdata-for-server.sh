f4fb551e114346b79624bcf3e1d31b9b5b8bb831
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-04-23 21:09:22 +0000

    Skript modifiziert.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4376 784bf6b6-0765-11df-962c-b5c252e50527

--- Skripte/create-serverdata-for-server.sh
+++ Skripte/create-serverdata-for-server.sh
@@ -3,3 +3,5 @@ mkdir /home/manaserv/data/
 cp ../../client-data/* /home/manaserv/data/
 cp -R ../../client-data/maps/ /home/manaserv/data/
 cp -R ../../server-data/scripts/ /home/manaserv/data/
+find /home/manaserv/data/ -type d -exec chmod 777 {} +  
+find /home/manaserv/data/ -type f -exec chmod 777 {} +  
