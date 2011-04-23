d9cdf2e1480dcb2202e02c0f21836b28957edb6d
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-04-23 21:07:54 +0000

    Weitere Hilfsskript hinzugefügt.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4375 784bf6b6-0765-11df-962c-b5c252e50527

---
+++ Skripte/create-serverdata-for-server.sh
@@ -0,0 +1,5 @@
+rm -f /home/manaserv/data/
+mkdir /home/manaserv/data/
+cp ../../client-data/* /home/manaserv/data/
+cp -R ../../client-data/maps/ /home/manaserv/data/
+cp -R ../../server-data/scripts/ /home/manaserv/data/
