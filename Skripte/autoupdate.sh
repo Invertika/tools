b69339cf64724396f9d0f638bd6bab8b934ba18c
Jurkan@gmx.de <Jurkan@gmx.de@784bf6b6-0765-11df-962c-b5c252e50527>
2011-05-31 15:52:24 +0000

    Autoupdate-Skript sollte jetzt nicht mehr ohne Commit losgehen
    git-svn-id: https://invertika.googlecode.com/svn/trunk@4630 784bf6b6-0765-11df-962c-b5c252e50527

--- Skripte/autoupdate.sh
+++ Skripte/autoupdate.sh
@@ -8,7 +8,15 @@ do
     sleep 60
     REVISION_SERVER=`svn info http://invertika.googlecode.com/svn/trunk | grep Revision|awk '{print $2}'`
     REVISION_LOCAL=`svn info ~/invertika/trunk | grep Revision|awk '{print $2}'` # Pfad anpassen
-    if [ $REVISION_SERVER != $REVISION_LOCAL ] 
+    let REVISION_SERVER_INT=$REVISION_SERVER
+    let REVISION_LOCAL_INT=$REVISION_LOCAL
+    if [ $REVISION_LOCAL_INT -eq 0 ]
+    then
+        False # Nix tun
+    elif [ $REVISION_SERVER_INT -eq 0 ]
+    then
+        False # Nix tun
+    elif [ $REVISION_SERVER != $REVISION_LOCAL ] 
     then
         mono autoupdate.exe autoupdate.xml
         sleep 600
