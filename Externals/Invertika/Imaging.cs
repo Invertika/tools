0b62dcccb9b1af8ca4b73f4cabb7337976d5cd26
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-08-15 14:23:58 +0000

    Weitere Funktionalität in die Bibliothek ausgelagert.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@5200 784bf6b6-0765-11df-962c-b5c252e50527

--- Externals/Invertika/Imaging.cs
+++ Externals/Invertika/Imaging.cs
@@ -6,11 +6,13 @@ using CSCL.FileFormats.TMX;
 using System.Drawing;
 using CSCL.Games.Manasource;
 using Invertika.Classes;
+using CSCL;
 
 namespace Invertika
 {
 	public static class Imaging
 	{
+		#region Weltkarte
 		/// <summary>
 		/// Speichert eine Karte welche je nach Monsterstärke eingefärbt wird
 		/// </summary>
@@ -99,5 +101,127 @@ namespace Invertika
 			tmpImage.Draw(0, 0, tmpDraw, true);
 			tmpImage.SaveToFile(filename);
 		}
+		#endregion
+
+		#region Kartenerzeugung
+		/// <summary>
+		/// Erzeugt aus einer Bitmap Außenkarten
+		/// </summary>
+		public static void CreateMapsFromBitmap(string filenameBitmap, string mapstemplates, string targetFolder, int xMin, int yMax, Progress progress)
+		{
+			if(progress!=null) if(!progress(0)) { return; }
+
+			string fnBitmap=filenameBitmap;
+			string pathMapsTemplates=mapstemplates;
+			//string pathMapsTemplates=Globals.folder_clientdata_mapstemplates;
+			string pathOutput=targetFolder;
+			//int MapX=(int)(decimal)nudXmin.Value;
+			//int MapY=(int)(decimal)nudYmax.Value;
+			int MapX=xMin;
+			int MapY=yMax;
+
+			//Folder
+			FileSystem.CreateDirectory(pathOutput, true);
+
+			//Datei laden
+			gtImage tmp=gtImage.FromFile(fnBitmap);
+
+			//Vars
+			uint kSize=100;
+
+			uint y=0;
+			uint x=0;
+
+			string TemplateMap="";
+
+			//FortschrittMax=(int)tmp.Height;
+			//FortschrittValue=0;
+			progress(0);
+			//if(i%fivePercent==0) if(pi!=null) if(!pi(i*100.0/header.NumberOfPointRecords)) return null;
+
+			while(y<tmp.Height)
+			{
+				progress(y/tmp.Height);
+				//FortschrittValue=(int)y;
+				//backgroundWorker.ReportProgress(0);
+
+				Console.Write(".");
+				x=0;
+				MapX=-25;
+
+				while(x<tmp.Width)
+				{
+					gtImage sub=tmp.GetSubImage(x, y, kSize, kSize);
+					Color col=sub.GetMedianColor();
+
+					#region Farbquantisierung
+					int tol=50;
+
+					//Meer
+					if(ColCheck(col, 118, 134, 165, tol))
+					{
+						TemplateMap="ow-meer.tmx";
+					}
+
+					//Wüste
+					if(ColCheck(col, 250, 203, 144, tol))
+					{
+						TemplateMap="ow-wueste.tmx";
+					}
+
+					//Eis
+					if(ColCheck(col, 254, 254, 254, tol+50))
+					{
+						TemplateMap="ow-eis.tmx";
+					}
+
+					//Wald
+					if(ColCheck(col, 33, 119, 31, tol))
+					{
+						TemplateMap="ow-wald.tmx";
+					}
+
+					//Erde
+					if(ColCheck(col, 152, 108, 64, tol))
+					{
+						TemplateMap="ow-erde.tmx";
+					}
+					#endregion
+
+					FileSystem.CopyFile(pathMapsTemplates+TemplateMap, pathOutput+Map.GetOuterWorldMapFilenameWithoutExtension(MapX, MapY, 0)+".tmx");
+
+					x+=kSize;
+					MapX++;
+				}
+
+				y+=kSize;
+				MapY--;
+			}
+
+			if(progress!=null) progress(100);
+		}
+
+		static bool ColCheck(Color col, int r, int g, int b, int tol)
+		{
+			if(ColTol(col.R, r, tol))
+			{
+				if(ColTol(col.G, g, tol))
+				{
+					if(ColTol(col.B, b, tol))
+					{
+						return true;
+					}
+				}
+			}
+
+			return false;
+		}
+
+		static bool ColTol(int c, int value, int tol)
+		{
+			if(value-tol<c&&value+tol>c) return true;
+			return false;
+		}
+		#endregion
 	}
 }
--- Invertika Editor/FormCreateMapsFromBitmap.cs
+++ Invertika Editor/FormCreateMapsFromBitmap.cs
@@ -8,6 +8,7 @@ using System.Windows.Forms;
 using CSCL;
 using CSCL.Graphic;
 using CSCL.Games.Manasource;
+using Invertika;
 
 namespace Invertika_Editor
 {
@@ -30,88 +31,16 @@ namespace Invertika_Editor
 			backgroundWorker.RunWorkerAsync();
 		}
 
+		bool SendProgress(double percent)
+		{
+			if(backgroundWorker==null) return true;
+			backgroundWorker.ReportProgress((int)percent);
+			return !backgroundWorker.CancellationPending;
+		}
+
 		private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
 		{
-			string fnBitmap=tbBitmap.Text;
-			string pathMapsTemplates=Globals.folder_clientdata_mapstemplates;
-			string pathOutput=tbTargetPath.Text;
-			int MapX=(int)(decimal)nudXmin.Value;
-			int MapY=(int)(decimal)nudYmax.Value;
-
-			//Folder
-			FileSystem.CreateDirectory(pathOutput, true);
-
-			//Datei laden
-			gtImage tmp=gtImage.FromFile(fnBitmap);
-
-			//Vars
-			uint kSize=100;
-
-			uint y=0;
-			uint x=0;
-
-			string TemplateMap="";
-
-			FortschrittMax=(int)tmp.Height;
-			FortschrittValue=0;
-
-			while(y<tmp.Height)
-			{
-				FortschrittValue=(int)y;
-				backgroundWorker.ReportProgress(0);
-
-				Console.Write(".");
-				x=0;
-				MapX=-25;
-
-				while(x<tmp.Width)
-				{
-					gtImage sub=tmp.GetSubImage(x, y, kSize, kSize);
-					Color col=sub.GetMedianColor();
-
-					#region Farbquantisierung
-					int tol=50;
-
-					//Meer
-					if(ColCheck(col, 118, 134, 165, tol))
-					{
-						TemplateMap="ow-meer.tmx";
-					}
-
-					//Wüste
-					if(ColCheck(col, 250, 203, 144, tol))
-					{
-						TemplateMap="ow-wueste.tmx";
-					}
-
-					//Eis
-					if(ColCheck(col, 254, 254, 254, tol+50))
-					{
-						TemplateMap="ow-eis.tmx";
-					}
-
-					//Wald
-					if(ColCheck(col, 33, 119, 31, tol))
-					{
-						TemplateMap="ow-wald.tmx";
-					}
-
-					//Erde
-					if(ColCheck(col, 152, 108, 64, tol))
-					{
-						TemplateMap="ow-erde.tmx";
-					}
-					#endregion
-
-					FileSystem.CopyFile(pathMapsTemplates+TemplateMap, pathOutput+Map.GetOuterWorldMapFilenameWithoutExtension(MapX, MapY, 0)+".tmx");
-
-					x+=kSize;
-					MapX++;
-				}
-
-				y+=kSize;
-				MapY--;
-			}
+			Imaging.CreateMapsFromBitmap(tbBitmap.Text, Globals.folder_clientdata_mapstemplates, tbTargetPath.Text, (int)nudXmin.Value, (int)nudYmax.Value, SendProgress);
 		}
 
 		private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
@@ -124,28 +53,6 @@ namespace Invertika_Editor
 		{
 			btnStartProcess.Enabled=true;
 			MessageBox.Show("Vorgang abgeschlossen!", "Hinweis", MessageBoxButtons.OK, MessageBoxIcon.Information);
-		}
-
-		static bool ColCheck(Color col, int r, int g, int b, int tol)
-		{
-			if(ColTol(col.R, r, tol))
-			{
-				if(ColTol(col.G, g, tol))
-				{
-					if(ColTol(col.B, b, tol))
-					{
-						return true;
-					}
-				}
-			}
-
-			return false;
-		}
-
-		static bool ColTol(int c, int value, int tol)
-		{
-			if(value-tol<c&&value+tol>c) return true;
-			return false;
 		}
 
 		private void btnBrowseBitmap_Click(object sender, EventArgs e)
