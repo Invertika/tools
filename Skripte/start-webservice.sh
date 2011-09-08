cad82ea64ad114bc91a093072b8b7f7bac66f2df
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-09-08 12:48:56 +0000

    Webservice Skript angepasst.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@5313 784bf6b6-0765-11df-962c-b5c252e50527

--- Skripte/start-webservice.sh
+++ Skripte/start-webservice.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-  python3 webservice.py 2> last_crash_webservice_log.txt
+  screen python3 webservice.py 2> last_crash_webservice_log.txt
   echo "Letzter Webservicecrash am `date`" > last_crash_webservice.txt
   sleep 5
 done
