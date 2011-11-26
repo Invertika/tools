0c2460c3947f7cbd941ce9f31d44945ba8c513ba
seeseekey <seeseekey@784bf6b6-0765-11df-962c-b5c252e50527>
2011-11-26 17:33:59 +0000

    Fehlende Enters eingefügt.
    git-svn-id: https://invertika.googlecode.com/svn/trunk@6197 784bf6b6-0765-11df-962c-b5c252e50527

--- ivktool/Program.cs
+++ ivktool/Program.cs
@@ -26,7 +26,7 @@ namespace ivktool
 	{
 		static void DisplayHelp()
 		{
-			Console.WriteLine("ivktool 1.8.3");
+			Console.WriteLine("ivktool 1.8.4");
 			Console.WriteLine("(c) 2008-2011 by the Invertika Developer Team (http://invertika.org)");
 			Console.WriteLine("");
 			Console.WriteLine("Nutzung: ivktool -aktion -parameter");
@@ -635,12 +635,12 @@ namespace ivktool
 
 				if(map.ObjectLayers.Count==0)
 				{
-					msg+=String.Format("Kein Objektlayer in der Map {0} vorhanden.", fn);
+					msg+=String.Format("Kein Objektlayer in der Map {0} vorhanden.\n", fn);
 				}
 
 				if(map.ObjectLayers.Count>1)
 				{
-					msg+=String.Format("Mehr als ein Objektlayer in der Map {0} vorhanden.", fn);
+					msg+=String.Format("Mehr als ein Objektlayer in der Map {0} vorhanden.\n", fn);
 				}
 
 				foreach(Objectgroup og in map.ObjectLayers)
