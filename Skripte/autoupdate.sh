ca2d10918660d704af5fb0e582c767dfb44cbdb6
seeseekey <seeseekey@googlemail.com>
2011-12-14 22:19:26 +0100

    Autoupdate Skript weiter angepasst.

--- Skripte/autoupdate.sh
+++ Skripte/autoupdate.sh
@@ -3,6 +3,7 @@
 ##Start with "./autoupdate.sh &"
 REPOSITORY="/home/manaserv/invertika/data/"
 AUTOUPDATE_EXE_FILENAME="/home/manaserv/autoupdate.exe"
+AUTOUPDATE_XML_FILENAME="/home/manaserv/autoupdate.xml"
 
 cd $REPOSITORY
 
@@ -14,8 +15,7 @@ do
     num_of_changes=`(cd $REPOSITORY && git diff HEAD origin/master|wc -l)`
     [ $num_of_changes != 0 ] && {
         (cd $REPOSITORY && git pull origin master)
-        mono $AUTOUPDATE_EXE_FILENAME autoupdate.xml
+        mono $AUTOUPDATE_EXE_FILENAME $AUTOUPDATE_XML_FILENAME
         sleep 600
     }
-done 
-
+done
